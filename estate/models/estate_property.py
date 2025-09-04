ba0d14554e45575096d5624439d58ea44af9ed2c
ThibClnt <tcolnot@ixemelis.fr>
2025-09-04 16:50:35 +0200

    Server framework 101: Chapter 6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -9,15 +9,16 @@ class EstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
-        copy=False, default=fields.Date.add(fields.Date.today(), months=+3))
+        copy=False, default=fields.Date.add(fields.Date.today(), months=+3),
+        string="Avaiable From")
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('south', 'South'),
